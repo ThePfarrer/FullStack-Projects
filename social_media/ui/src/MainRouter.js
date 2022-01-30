43b7c9f9e18d1d8570031a9fbd2173d161a6ad9f
Uche Okereke <orezpablo@gmail.com>
2022-01-30 01:07:40 +0100

    feat: add signup component

--- social_media/ui/src/MainRouter.js
+++ social_media/ui/src/MainRouter.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./core/Home";
+import SignUp from "./user/Signup";
 import Users from "./user/Users";
 
 const MainRouter = () => {
@@ -8,6 +9,7 @@ const MainRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </div>
   );
